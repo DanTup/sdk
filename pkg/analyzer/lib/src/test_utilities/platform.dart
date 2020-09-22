bb3a134d5d5a5f97422df5aeba9992a8136f84a2
Danny Tuppeny <danny@tuppeny.com>
2020-09-22 15:18:59 +0100

    Review tweaks

--- pkg/analyzer/lib/src/test_utilities/platform.dart
+++ pkg/analyzer/lib/src/test_utilities/platform.dart
@@ -1,3 +1,7 @@
+// Copyright (c) 2020, the Dart project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 import 'dart:io';
 
 /// The EOL character to be used for source code in tests.
@@ -7,6 +11,12 @@ final platformEol = Platform.isWindows ? '\r\n' : '\n';
 /// when running on Windows \r\n is used even though source files are checked
 /// out using \n.
 String normalizeNewlinesForPlatform(String input) {
+  // Skip normalising for other platforms, as the gitattributes for the SDK
+  // will ensure all files are \n.
+  if (!Platform.isWindows) {
+    return input;
+  }
+
   // TODO(dantup): This is commited as a no-op because it breaks many tests
   // on Windows.
   //
