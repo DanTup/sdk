580d6f7ccc7af12ad591bf480beec784eca51721
Danny Tuppeny <danny@tuppeny.com>
2026-07-29 21:19:55 +0100

    Add temporary test in test/ that runs the diagnostic tests from integration_test/

--- pkg/analysis_server/test/lsp/test_all.dart
+++ pkg/analysis_server/test/lsp/test_all.dart
@@ -62,6 +62,7 @@ import 'snippets_test.dart' as snippets;
 import 'source_edits_test.dart' as source_edits;
 import 'summary_test.dart' as summary;
 import 'super_test.dart' as get_super;
+import 'temporary_integration_test.dart' as temporary_integration;
 import 'temporary_overlay_operation_test.dart' as temporary_overlay_operation;
 import 'type_definition_test.dart' as type_definition;
 import 'type_hierarchy_test.dart' as type_hierarchy;
@@ -131,6 +132,7 @@ void main() {
     signature_help.main();
     snippets.main();
     source_edits.main();
+    temporary_integration.main();
     temporary_overlay_operation.main();
     type_definition.main();
     type_hierarchy.main();
